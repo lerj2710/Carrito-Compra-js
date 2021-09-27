9ae8780c64cc7c683f8bddb38448b833d1739a2f
luis rodriguez <kike.lerj2710@gmail.com>
2021-09-27 08:24:35 -0500

    aumentar la cantidad y que no se duplique

--- js/app.js
+++ js/app.js
@@ -30,13 +30,31 @@ function leerDatoCurso(curso) {
        // crear un objecto que muestre la informacion del curso
     const infoCurso = {
         imagen : curso.querySelector('img').src,
+        imagen: curso.querySelector('img').src,
         titulo : curso.querySelector('h4').textContent,
         precio : curso.querySelector('.precio span').textContent,
         id     : curso.querySelector('a').getAttribute('data-id'),//seleccionando el id desde el html
         cantidad: 1
     };
-    //Agregar elementos al carrito
-    articulosCarrito = [...articulosCarrito, infoCurso];// usando express operartor
+
+    // verificar si un carrito tiene un objeto duplicado
+    const existe = articulosCarrito.some( curso => curso.id === infoCurso.id);
+        if(existe){
+            //Actulizar la cantidad
+            const cursos = articulosCarrito.map( curso =>{
+               
+                if (curso.id === infoCurso.id) {
+                    curso.cantidad++;
+                    return curso;//retorna el objeto actulizado
+                }else{
+                    return curso;// // retorna el objeto no son duplicado
+                }
+            });
+            articulosCarrito = [...cursos];
+        }else{
+            //Agregar elementos al carrito
+            articulosCarrito = [...articulosCarrito, infoCurso];// usando express operartor
+        }             
     console.log(articulosCarrito);
     carritoHTML();
 }
@@ -50,19 +68,21 @@ function carritoHTML() {
         limpiarHtml();
     articulosCarrito.forEach( curso => {// iretar sobre cada objeto mas rapido
         //usar distructurar objecto
-        const {imagen, titulo,precio,id}=curso;
+        const { imagen, titulo,precio,id,cantidad}=curso;
         //generar el HTML
-        console.log(curso)
         const row = document.createElement('tr');
         row.innerHTML= `
-            <td>
-               <img src" ${imagen}" width="100">
+            <td>  
+               <img src="${imagen}" width=100>
             </td>
             <td>
                 ${titulo}
             </td>
             <td>
                 ${precio}
+            </td>
+            <td>
+                ${cantidad}
             </td>
             <td>
                 <a href="#" class="borrar-curso" data-id="${id}">X</a>
