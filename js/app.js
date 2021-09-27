fdc7f44dda1b72bdf93a8538f829b8a2df778a33
luis rodriguez <kike.lerj2710@gmail.com>
2021-09-26 21:29:37 -0500

    usando un arreglo para los objetos y mostrar en el HTML

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector('#carrito');// selecionando el div del ca
 const contedorCarrito = document.querySelector('#lista-carrito tbody');// donde se va almacenar los carritos
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');// donde puedo vaciar todo los productos
 const listaCursos = document.querySelector('#lista-cursos'); // div donde estan los cursos de los productos
+let articulosCarrito = [];
 
 //============== Resgistrar los eventos=================
 cargarEventListeners();
@@ -24,13 +25,34 @@ function agregarCurso(e) {
 
 //leer el contenido HTML y al que le dimos click
 function leerDatoCurso(curso) {
-    console.log(curso)
+    // console.log(curso)
     // crear un objecto que muestre la informacion del curso
     const infoCurso = {
         imagen : curso.querySelector('img').src,
         titulo : curso.querySelector('h4').textContent,
         precio : curso.querySelector('.precio span').textContent,
         id     : curso.querySelector('a').getAttribute('data-id'),//seleccionando el id desde el html
+        cantidad: 1
     };
-    console.log(infoCurso);
+    //Agregar elementos al carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];// usando express operartor
+    // console.log(articulosCarrito);
+    carritoHTML();
+}
+
+
+// Mostar el contenido en el HTML de el carrito
+
+function carritoHTML() {
+
+    articulosCarrito.forEach( curso => {// iretar sobre cada objeto mas rapido
+        const row = document.createElement('tr');
+        row.innerHTML= `
+        <td>
+            ${curso.titulo}
+        </td>
+        `;
+        //Agrega el HTML del carrito al tbody
+        contedorCarrito.appendChild(row);
+    })
 }
